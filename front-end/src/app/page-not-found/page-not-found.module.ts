804cfee0a5848b83acd936e54bf9b2913fe8338e
Riddlegun <Riddlegun@gmail.com>
2017-07-08 00:15:05 +0300

    page not found view done

--- front-end/src/app/page-not-found/page-not-found.module.ts
+++ front-end/src/app/page-not-found/page-not-found.module.ts
@@ -1,11 +1,17 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {MdButtonModule} from '@angular/material';
+import {RouterModule} from '@angular/router';
 
 import {PageNotFoundComponent} from './page-not-found.component';
 
 @NgModule({
   declarations: [PageNotFoundComponent],
-  imports: [CommonModule]
+  imports: [
+    CommonModule,
+    MdButtonModule,
+    RouterModule
+  ]
 })
 export class PageNotFoundModule {
 }
