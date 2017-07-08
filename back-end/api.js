9f79a4097f542a0a7b67c8e112c200e7426e4f91
Riddlegun <Riddlegun@gmail.com>
2017-07-08 17:20:08 +0300

    dummy checkAuth added, api request handlers fixed

--- back-end/api.js
+++ back-end/api.js
@@ -4,19 +4,34 @@ const Operator = require('./models/operator');
 
 function initialize(app) {
   app.get('/api/dialogs', (req, res, next) => {
-    Dialog.find({}, (err, dialogs) => {
-      if (err) next(err);
+    checkAuth(req, next).then((customerId) => {
+      if (!customerId) res.status(401).send('Unauthorized');
 
-      res.status(200).send(dialogs);
+      getOperatorsAndDialogs(customerId, res, next)
+        .then((result) => {
+          if (!result || !result.dialogs) {
+            res.status(404).send('Not found');
+          }
+          res.status(200).send(result.dialogs);
+        });
     });
   });
 
   app.get('/api/dialog/:id', (req, res, next) => {
     if (req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
-      Dialog.findById(req.params.id, (err, doc) => {
-        if (err) next(err);
+      checkAuth(req, next).then((customerId) => {
+        if (!customerId) res.status(401).send('Unauthorized');
 
-        res.status(200).send(doc);
+        Dialog.findById(req.params.id, (err, dialog) => {
+          if (err) next(err);
+          if (!dialog) {
+            res.status(404).send('Not found');
+            return;
+          }
+
+          res.status(200).send(dialog);
+        });
+
       });
     } else {
       res.status(400).send('Bad request');
@@ -24,28 +39,95 @@ function initialize(app) {
   });
 
   app.get('/api/operators', (req, res, next) => {
-    Dialog.find().lean().exec((err, dialogs) => {
-      if (err) next(err);
+    checkAuth(req, next).then((customerId) => {
+      if (!customerId) res.status(401).send('Unauthorized');
 
-      Operator.find().lean().exec((err, operators) => {
-        if (err) next(err);
+      getOperatorsAndDialogs(customerId, res, next)
+        .then((result) => {
+          if (!result) return;
+          if (!result.dialogs) {
+            res.status(200).send(result.operators);
+            return;
+          }
 
-        operators.forEach((operator) => {
-          operator.dialogs = [];
+          const dialogs = result.dialogs;
+          const operators = result.operators;
 
-          dialogs.forEach((dialog) => {
-            if (dialog.operatorId.equals(operator._id)) {
-              operator.dialogs.push(dialog);
-            }
+          operators.forEach((operator) => {
+            operator.dialogs = [];
+
+            dialogs.forEach((dialog) => {
+              if (dialog.operatorId.equals(operator._id)) {
+                operator.dialogs.push(dialog);
+              }
+            });
           });
-        });
 
-        res.status(200).send(operators);
-      });
+          res.status(200).send(operators);
+        });
     });
   });
 
   // add another API methods here
+}
+
+function checkAuth(req, next) {
+  /*const token = req.cookies.token;
+
+  return new Promise((resolve, reject) => {
+    if (token) {
+      Customer.findOne({token: token}, {_id: 1}, (err, customer) => {
+        if (err) next(err);
+        resolve(customer._id);
+      });
+    } else {
+      resolve(null);
+    }
+  });*/
+
+  // TODO: implement real auth
+
+  return new Promise((resolve) => {
+    Customer.findOne({}, (err, docs) => {
+      resolve(docs._id);
+    });
+  });
+}
+
+function getOperatorsAndDialogs(customerId, res, next) {
+  return new Promise((resolve) => {
+    Operator.find({customerId: customerId}).lean().exec((err, operators) => {
+      if (err) next(err);
+      if (!operators.length) {
+        res.status(404).send('Not found');
+        resolve(null);
+        return;
+      }
+
+      const operatorIds = [];
+
+      operators.forEach((operator) => {
+        operatorIds.push(operator._id);
+      });
+
+      Dialog.find({
+        operatorId: { $in: operatorIds }
+      }).lean().exec((err, dialogs) => {
+        if (err) next(err);
+        if (!dialogs.length) {
+          resolve({
+            operators: operators
+          });
+          return;
+        }
+
+        resolve({
+          dialogs: dialogs,
+          operators: operators
+        });
+      });
+    });
+  });
 }
 
 module.exports = initialize;
