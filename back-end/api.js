bb223c947a27eebfe6990fd10b67cf8de914cafb
Riddlegun <Riddlegun@gmail.com>
2017-07-08 22:18:46 +0300

    api request handlers refactoring

--- back-end/api.js
+++ back-end/api.js
@@ -4,80 +4,136 @@ const Operator = require('./models/operator');
 
 function initialize(app) {
   app.get('/api/dialogs', (req, res, next) => {
-    checkAuth(req, next).then((customerId) => {
-      if (!customerId) res.status(401).send('Unauthorized');
+    checkAuth(req)
+      .then((customerId) => {
+        if (!customerId) {
+          res.status(401).send('Unauthorized');
+          return;
+        }
 
-      getOperatorsAndDialogs(customerId, res, next)
-        .then((result) => {
-          if (!result || !result.dialogs) {
-            res.status(404).send('Not found');
-          }
-          res.status(200).send(result.dialogs);
-        });
-    });
+        getOperators(customerId)
+          .then((operators) => {
+            if (!operators) {
+              res.status(404).send('Not found');
+              return;
+            }
+            const operatorIds = [];
+
+            operators.forEach((operator) => {
+              operatorIds.push(operator._id);
+            });
+
+            getDialogs(operatorIds)
+              .then((dialogs) => {
+                if (!dialogs) {
+                  res.status(404).send('Not found');
+                  return;
+                }
+
+                res.status(200).send(dialogs);
+              })
+              .catch((err) => {
+                next(err);
+              });
+          })
+          .catch((err) => {
+            next(err);
+          });
+      })
+      .catch((err) => {
+        next(err);
+      });
   });
 
   app.get('/api/dialog/:id', (req, res, next) => {
     if (req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
-      checkAuth(req, next).then((customerId) => {
-        if (!customerId) res.status(401).send('Unauthorized');
+      checkAuth(req, next)
+        .then((customerId) => {
+          if (!customerId) res.status(401).send('Unauthorized');
 
-        Dialog.findById(req.params.id, (err, dialog) => {
-          if (err) next(err);
-          if (!dialog) {
-            res.status(404).send('Not found');
-            return;
-          }
+          Dialog.findById(req.params.id, (err, dialog) => {
+            if (err) next(err);
+            if (!dialog) {
+              res.status(404).send('Not found');
+              return;
+            }
 
-          res.status(200).send(dialog);
+            res.status(200).send(dialog);
+          });
+        })
+        .catch((err) => {
+          next(err);
         });
-
-      });
     } else {
       res.status(400).send('Bad request');
     }
   });
 
   app.get('/api/operators', (req, res, next) => {
-    checkAuth(req, next).then((customerId) => {
-      if (!customerId) res.status(401).send('Unauthorized');
+    checkAuth(req, next)
+      .then((customerId) => {
+        if (!customerId) {
+          res.status(401).send('Unauthorized');
+          return;
+        }
 
-      getOperatorsAndDialogs(customerId, res, next)
-        .then((result) => {
-          if (!result) return;
-          if (!result.dialogs) {
-            res.status(200).send(result.operators);
-            return;
-          }
+        getOperators(customerId)
+          .then((operators) => {
+            if (!operators) {
+              res.status(404).send('Not found');
+              return;
+            }
+            const operatorIds = [];
 
-          const dialogs = result.dialogs;
-          const operators = result.operators;
-
-          operators.forEach((operator) => {
-            operator.dialogs = [];
-
-            dialogs.forEach((dialog) => {
-              if (dialog.operatorId.equals(operator._id)) {
-                operator.dialogs.push(dialog);
-              }
+            operators.forEach((operator) => {
+              operatorIds.push(operator._id);
             });
-          });
 
-          res.status(200).send(operators);
-        });
-    });
+            getDialogs(operatorIds)
+              .then((dialogs) => {
+                if (!dialogs) {
+                  res.status(200).send(operators);
+                  return;
+                }
+
+                operators.forEach((operator) => {
+                  operator.dialogs = [];
+
+                  dialogs.forEach((dialog) => {
+                    if (dialog.operatorId.equals(operator._id)) {
+                      operator.dialogs.push(dialog);
+                    }
+                  });
+                });
+
+                res.status(200).send(operators);
+              })
+              .catch((err) => {
+                next(err);
+              });
+          })
+          .catch((err) => {
+            next(err);
+          });
+      })
+      .catch((err) => {
+        next(err);
+      });
   });
 
   // add another API methods here
 }
 
-function checkAuth(req, next) {
+function checkAuth(req) {
   /*const token = req.cookies.token;
 
   return new Promise((resolve, reject) => {
     if (token) {
       Customer.findOne({token: token}, {_id: 1}, (err, customer) => {
-        if (err) next(err);
+        if (err) {
+          reject(err);
+          return;
+        }
         resolve(customer._id);
       });
     } else {
@@ -87,46 +143,72 @@ function checkAuth(req, next) {
 
   // TODO: implement real auth
 
-  return new Promise((resolve) => {
-    Customer.findOne({}, (err, docs) => {
-      resolve(docs._id);
-    });
-  });
-}
-
-function getOperatorsAndDialogs(customerId, res, next) {
-  return new Promise((resolve) => {
-    Operator.find({customerId: customerId}).lean().exec((err, operators) => {
-      if (err) next(err);
-      if (!operators.length) {
-        res.status(404).send('Not found');
+  return new Promise((resolve, reject) => {
+    Customer.findOne({}, (err, customer) => {
+      if (err) {
+        reject(null);
+        return;
+      }
+      if (!customer) {
         resolve(null);
         return;
       }
 
-      const operatorIds = [];
+      resolve(customer._id);
+    });
+  });
+}
 
-      operators.forEach((operator) => {
-        operatorIds.push(operator._id);
-      });
+function getOperators(customerId) {
+  return new Promise((resolve, reject) => {
+    Operator.find({customerId: customerId}).lean().exec((err, operators) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      if (!operators.length) {
+        resolve(null);
+        return;
+      }
 
+      resolve(operators);
+    });
+  });
+}
+
+function getDialogs(query) {  // query: operatorId[] | operatorId
+  return new Promise((resolve, reject) => {
+    if (Array.isArray(query)) {
       Dialog.find({
-        operatorId: { $in: operatorIds }
+        operatorId: { $in: query }
       }).lean().exec((err, dialogs) => {
-        if (err) next(err);
+        if (err) {
+          reject(err);
+          return;
+        }
         if (!dialogs.length) {
-          resolve({
-            operators: operators
-          });
+          resolve(null);
           return;
         }
 
-        resolve({
-          dialogs: dialogs,
-          operators: operators
-        });
+        resolve(dialogs);
       });
-    });
+    } else {
+      Dialog.find({
+        operatorId: query
+      }).lean().exec((err, dialogs) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        if (!dialogs.length) {
+          resolve(null);
+          return;
+        }
+
+        resolve(dialogs);
+      });
+    }
   });
 }
 
