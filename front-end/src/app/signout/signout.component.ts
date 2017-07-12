2042e4f864b1e87d800b78956551c410baaab7d7
Riddlegun <Riddlegun@gmail.com>
2017-07-12 17:38:38 +0300

    signout storage clean up added

--- front-end/src/app/signout/signout.component.ts
+++ front-end/src/app/signout/signout.component.ts
@@ -1,12 +1,23 @@
 import {Component, OnInit} from '@angular/core';
 
+import {StorageService} from '../core/storage.service';
+
 @Component({
   templateUrl: './signout.component.html'
 })
 export class SignoutComponent implements OnInit {
-  constructor() {
+  constructor(private storage: StorageService) {
   }
 
   ngOnInit() {
+    this.storage.customerInfo = {
+      customerDetails: null,
+      dialogDetails: null,
+      dialogs: null,
+      operatorDetails: null,
+      operators: null,
+    };
+    this.storage.operatorInfo = null;
+    this.storage.userRole = null;
   }
 }
