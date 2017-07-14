96ebf67214fd3e5e258d7d1186703ceb8117cdd0
vladbulaniy <vladbulanyy@gmail.com>
2017-07-15 00:43:01 +0300

    implementation first bindig

--- front-end/angular-app/src/app/customer/dialog-details/dialog-details.component.ts
+++ front-end/angular-app/src/app/customer/dialog-details/dialog-details.component.ts
@@ -1,13 +1,29 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+
+
+import {ApiService} from '../../core/api.service';
 
 @Component({
   templateUrl: './dialog-details.component.html',
   styleUrls: ['./dialog-details.component.scss']
 })
 export class DialogDetailsComponent implements OnInit {
-  constructor() {
-  }
+  dialog:any;
+  constructor(
+    private Api: ApiService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.route.data
+      .subscribe((data) => {
+        this.dialog = data.dialog;
+        console.log('data', this.dialog);
+        if (!this.dialog){
+          this.router.navigate(['/page-not-found']);
+        }
+      })
   }
 }
